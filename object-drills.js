228dba26d0b29de047679ddea9a94ab9d966b29d
Matt Williamson <mathewbwilliamson@gmail.com>
2018-08-22 13:42:42 -0400

    fixed formatting issues

--- object-drills.js
+++ object-drills.js
@@ -1,13 +1,13 @@
 'use strict';
 function createMyObject() {
-    return {
-        foo: 'bar',
-        answerToUniverse: 42,
-        'olly olly': 'oxen free',
-        sayHello: function() {
-            return 'Hello';
-        }
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+        return 'Hello';
     }
+  }
 }
 
 let myObject = createMyObject();
@@ -16,10 +16,10 @@ console.log(myObject);
 /////////////////////////////////////////
 
 function updateObject(obj) {
-    obj.foo = 'foo';
-    obj['bar'] = 'bar';
-    obj.bizz = 'bizz';
-    obj.bang = 'bang';
+  obj.foo = 'foo';
+  obj['bar'] = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
 }
 
 updateObject(myObject);
@@ -27,14 +27,14 @@ console.log(myObject);
 
 /////////////////////////////////////////
 function personMaker() {
-    let person = {
-        firstName: 'Paul',
-        lastName: 'Jones',
-        fullName: function() {
-            return this.firstName + ' ' + this.lastName
-        },
-    };
-    return person;
+  let person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+        return this.firstName + ' ' + this.lastName
+    },
+  };
+  return person;
 }
 
 let myPerson = personMaker();
@@ -42,17 +42,17 @@ console.log(myPerson.fullName());
 
 /////////////////////////////////////////////
 const sampleObj = {
-    foo: 'foo',
-    bar: 'bar',
-    bizz: 'bizz',
-    bang: 'bang',
-  };
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
   
-  function keyDeleter(obj) {
-    delete obj.foo;
-    delete obj.bar;
-    return obj;
-  }
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
 
   /* From here down, you are not expected to 
    understand.... for now :)  
